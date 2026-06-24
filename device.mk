justinlin099/android_device_sony_lilac

Add Settings Google

device.mk
@@ -34,7 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/sony/lilac/overlay/packages/apps/Settings\
-    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI
+    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI\
+    device/sony/lilac/overlay/packages/apps/SettingsGoogle
 
 ### POWER
 TARGET_USE_CUSTOM_POWERHINT := true

VictorX6996/android_device_sony_poplar_kddi

device.mk
@@ -23,7 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/sony/poplar_kddi/overlay/packages/apps/Settings\
-    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI
+    device/sony/yoshino-common/overlay/frameworks/base/packages/SystemUI\
+    device/sony/poplar_kddi/overlay/packages/apps/SettingsGoogle
+
 
 ### POWER
 TARGET_USE_CUSTOM_POWERHINT := true